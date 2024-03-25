e4f1227c026c1ce04e9e4d443a49b14b2d942d2d
Mukhyajit Gogoi <mukhyajit11@gmail.com>
2024-03-26 02:56:54 +0530

    fixed Continue button, minor adjustments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// image animation
 document.addEventListener('DOMContentLoaded', function() {
     let images = document.querySelectorAll('.gallery img');
     let index = 0;
@@ -10,3 +11,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
     setInterval(showImage, 3000); // Change image every 3 seconds
 });
+
+// function to disable the "Continue" button if no option is selected
+function selectMovie() {
+    var selectBox = document.getElementById("selectMovie");
+    var continueButton = document.getElementById("continueButton");
+
+    // Enable the button only if a movie is selected
+    continueButton.disabled = selectBox.value === "";
+
+    // Change button color when a movie is selected
+    continueButton.style.backgroundColor = selectBox.value === "" ? "" : "azure";
+}
+
