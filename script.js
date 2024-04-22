8f574ce8e982d66a8d4117eb0de770ade96f3597
Mukhyajit Gogoi <mukhyajit11@gmail.com>
2024-04-23 03:34:26 +0530

    added separate booking page

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ let slideIndex = 0;
 let intervalId = null;
 
 document.addEventListener("DOMContentLoaded" ,initializeSlider); // calling the fn after the DOM content fully loads
-function initializeSlider(){ //for the first image 
 
-    slides[slideIndex].classList.add("displaySlide");
-    intervalId = setInterval(nextSlide, 5000); //goes to the next img every 5secs
+function initializeSlider(){ //for the first image 
+    if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 5000); //goes to the next img every 5secs
+    }
 }
 
 
@@ -24,7 +26,6 @@ function showSlide(index){
     });
     slides[slideIndex].classList.add("displaySlide"); // will display as a block
 }
-
 
 function prevSlide(){
     slideIndex--;
@@ -47,24 +48,9 @@ function selectMovie() {
 
     // Change button color when a movie is selected
     continueButton.style.backgroundColor = selectBox.value === "" ? "" : "azure";
+
+// function to store the selected movie name for the next page
+    const selectElement = document.getElementById('selectMovie');
+    const selectedValue = selectElement.value;
+    sessionStorage.setItem('selectedMovie', selectedValue); // Store selected value in sessionStorage
 }
-
-//Popup window
-const continueButton = document.getElementById('continueButton');
-const popup = document.getElementById('popup');
-const closeButton = document.getElementById('closeButton');
-// Function to display the popup
-function displayPopup() {
-    popup.style.display = 'block';
-}
-
-// Function to close the popup
-function closePopup() {
-    popup.style.display = 'none';
-}
-
-// Event listener for the continue button
-continueButton.addEventListener('click', displayPopup);
-
-// Event listener for the close button inside the popup
-closeButton.addEventListener('click', closePopup);
